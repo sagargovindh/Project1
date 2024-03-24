e5fa9b3d45f573fb41ce0ef16313550f4d13c55f
sagargovindh <163328467+sagargovindh@users.noreply.github.com>
2024-03-24 11:16:55 +0530

    changed a to 22

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
     public static void main(String[] args) {
-        int a=10;
+        int a=22;
         int b=200;
         System.out.println("hii Demo");
         System.out.println(a);
