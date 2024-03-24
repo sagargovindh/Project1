911a27fcf501d9d4944219ccadc087c9e031a96f
sagargovindh <211900343sagargovindh018@gmail.com>
2024-03-24 10:46:08 +0530

    firstcommit

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+    public static void main(String[] args) {
+        int a=100;
+        int b=200;
+        System.out.println("hii Demo");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
