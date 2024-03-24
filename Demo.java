357105924842dba3fdbc9945a4001926300e05e3
sagargovindh <211900343sagargovindh018@gmail.com>
2024-03-24 11:36:27 +0530

    change a to 200

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
     public static void main(String[] args) {
-        int a=20;
+        int a=200;
         int b=200;
         System.out.println("hii Demo");
         System.out.println(a);
